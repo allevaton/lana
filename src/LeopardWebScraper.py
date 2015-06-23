allevaton/lana

commit 81359250926ff32be818194a813419f1dfe2948f

Scraper is indeed working
Using requests and BeautifulSoup to POST to LeopardWeb and get session tokens to allow us to post to the Advanced Search and select all classes. This returns a HUGE HTML page with every class. We sift through this and turn it into a large JSON array and save it to a file.

qs file: A qs file is simply a file that stores the magic query string when posting to the Advanced Search. It's a huge form with many parameters we need to automate. This will likely change in a future implementation that generates this on the fly. The API will most likely not change, but implementation will.

More to come.

src/LeopardWebScraper.py
@@ -1,10 +1,15 @@
 #
 #
 #
+import json
+import os
+import sys
+
 
 __author__ = 'Nick Allevato'
 
 from getpass import getpass
+from urllib import parse
 
 from bs4 import BeautifulSoup
 import requests
@@ -16,29 +21,57 @@ class LeopardWebScraper(BaseScraper):
     name = 'Leopard Web Scraper'
     simple = 'wit'
 
-    requires_auth = True
-    session = None
+    _session = None
 
-    # login_url = 'http://leopardweb.wit.edu'
-    login_url = 'https://cas.wit.edu/cas/login'
+    _login_url = 'http://leopardweb.wit.edu'
+    _user_agent = ('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                   'Chrome/43.0.2357.125 Safari/537.36')
+    _session_post_args = {
+        'headers': {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': _user_agent
+        },
+        'verify': False,
+        'allow_redirects': True
+    }
 
-    def __init__(self, username, password):
+    _is_auth = False
+
+    def __init__(self):
+        pass
+
+    def get_qs(self):
+        """ Gets the magic query string for posting and getting the list of all classes
+        """
+        n = self.simple + '.qs'
+        if os.path.exists(n):
+            with open(n) as fp:
+                return fp.readline()
+        else:
+            # need to generate and save the new query string
+            if not self._session:
+                raise ConnectionError('You must be connected to generate a query string')
+
+                # TODO finish the ability to generate a query string
+
+    def connect(self):
+        self._session = requests.Session()
+        return True
+
+    def authenticate(self, username='', password=''):
+        # WIT makes this complicated and requires that a specific identifier is sent along.
+        # This identifier is uniquely generated per request and lives in the login page.
+        # We need to scrape this out, along with other possible information, before we can post.
+        if not self._session:
+            raise ConnectionError('You must be connected to authenticate')
+
         name = ' for ' + self.name if self.name else ''
         if not username:
             username = input('Enter username' + name + ': ')
         if not password:
             password = getpass('Enter password' + name + ': ')
 
-        if self.authenticate(username, password):
-            pass
-        print('test')
-
-    def authenticate(self, username, password):
-        # WIT makes this complicated and requires that a specific identifier is sent along.
-        # This identifier is uniquely generated per request and lives in the login page.
-        # We need to scrape this out, along with other possible information, before we can post.
-        self.session = requests.Session()
-        response = self.session.get(self.login_url, verify=False)
+        response = self._session.get(self._login_url, **self._session_post_args)
         soup = BeautifulSoup(response.text)
 
         payload = {
@@ -51,22 +84,61 @@ class LeopardWebScraper(BaseScraper):
             payload[hidden.attrs['name']] = hidden.attrs['value']
 
         bigurl = 'https://cas.wit.edu/cas/login?service=https%3A%2F%2Fprodweb2.wit.edu%3A443%2Fssomanager%2Fc%2FSSB'
-        response = self.session.post(bigurl, payload, verify=False)
+        response = self._session.post(bigurl, payload, **self._session_post_args)
+        del password
 
-        return False
+        soup = BeautifulSoup(response.text)
+        if len(soup.find_all('div', class_='errors')) > 0:
+            print('Authenticating %s failed' % self.name, file=sys.stderr)
+            return False
 
-    def scrape_data(self):
-        pass
+        self._is_auth = True
+        return True
+
+    def scrape_data(self, outfile_name=''):
+        if not self._session:
+            raise ConnectionError('You must be connected to scrape the data')
+
+        payload = parse.parse_qs(self.get_qs(), keep_blank_values=True)
+
+        response = self._session.post('https://prodweb2.wit.edu/SSBPROD/bwskfcls.P_GetCrse_Advanced',
+                                      payload, **self._session_post_args)
+        soup = BeautifulSoup(response.text)
+        data = []
+        headers = []
+        for c in soup.find(class_='datadisplaytable').find_all('tr'):
+            if not headers and c.find('th', class_='ddheader'):
+                headers = [head.text.lower() for head in c.find_all('th', class_='ddheader')]
+                headers = [h.split('(')[0].strip() for h in headers]
+                continue
+
+            if not c.find('td', class_='dddefault'):
+                continue
+
+            # Yes, these are the droids you were looking for.
+            d = self.cleanup_entry(dict(zip(headers, [t.text for t in c.find_all('td', class_='dddefault')])))
+            data.append(d)
+
+        if outfile_name:
+            with open(outfile_name, 'w') as fp:
+                fp.write(json.dumps(data))
+
+        return data
+
+    def cleanup_entry(self, class_row):
+        class_row['instructor'] = class_row['instructor'].split('(')[0].strip()
+        class_row['attribute'] = class_row['attribute'].strip()
+        return class_row
 
     def __del__(self):
         pass
-
-    disconnect = __del__
 
 
 if __name__ == '__main__':
     # run the leopard web scraper
     un = input('Enter username: ')
-    pw = getpass('Enter password: ')
-    scraper = LeopardWebScraper(un, pw)
-    pass
+    # pw = getpass('Enter password: ')
+
+    scraper = LeopardWebScraper()
+    if scraper.connect() and scraper.authenticate(un):
+        scraper.scrape_data('wit.json')

src/BaseScraper.py
@@ -11,13 +11,16 @@ from getpass import getpass
 
 
 class BaseScraper():
-    name = ''
+    name = 'Unidentified Scraper'
+    simple = ''
+
+    _is_auth = False
 
     @abstractmethod
     def __str__(self):
         """ Returns the name of the scraper
         """
-        return 'Undefined Scraper'
+        return self.name
 
     @abstractmethod
     def connect(self):
@@ -39,6 +42,6 @@ class BaseScraper():
 
     @abstractmethod
     def scrape_data(self):
-        """ Returns a list of ClassData objects
+        """ TODO design
         """
         return []

src/wit.qs
@@ -1,0 +1,1 @@
+rsts=dummy&crn=dummy&term_in=201610&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj=ARCH&sel_subj=BIOL&sel_subj=BMED&sel_subj=BLDG&sel_subj=TBAN&sel_subj=CHEM&sel_subj=TCNA&sel_subj=CIVE&sel_subj=COMM&sel_subj=COMP&sel_subj=TCON&sel_subj=TCLI&sel_subj=CONM&sel_subj=TCMC&sel_subj=COOP&sel_subj=DSGN&sel_subj=ETHS&sel_subj=ECON&sel_subj=ELMC&sel_subj=ELEC&sel_subj=TEIT&sel_subj=ENGR&sel_subj=ENGL&sel_subj=FMGT&sel_subj=TFMC&sel_subj=FYS&sel_subj=HIST&sel_subj=HUMN&sel_subj=INDS&sel_subj=INTD&sel_subj=TJEC&sel_subj=LITR&sel_subj=TMTO&sel_subj=MGMT&sel_subj=MANF&sel_subj=MATH&sel_subj=MECH&sel_subj=TCAD&sel_subj=PHIL&sel_subj=PHYS&sel_subj=POLS&sel_subj=TPRC&sel_subj=TPBS&sel_subj=TPMC&sel_subj=PSYC&sel_subj=SOCL&sel_subj=SURV&sel_subj=TMGT&sel_subj=TWEL&sel_crse=&sel_title=&sel_schd=%25&sel_from_cred=&sel_to_cred=&sel_levl=%25&sel_ptrm=%25&sel_instr=%25&sel_attr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a&SUB_BTN=Section+Search&path=1

commit 9616935fbf937050b2d36db6e7edc730e0597821

Working commit
The decorators from 81359250926ff32be818194a813419f1dfe2948f caused a problem with nesting and we are unable to use them as planned. This isn't a big problem, but it will lead to possible inconsistencies in error checking. Maybe we can find a way around it, but it may not involve decorators. They have been removed and things should be working as planned.

src/LeopardWebScraper.py
@@ -14,27 +14,6 @@ import requests
 from BaseScraper import BaseScraper
 
 
-def requires_auth(func, failed_message=''):
-    def magic(self, *args, **kwargs):
-        if not self._is_auth:
-            raise PermissionError(failed_message or 'You must be authenticated to do this')
-        else:
-            func(*args, **kwargs)
-
-    return magic
-
-
-def requires_connection(func, failed_message=''):
-    def magic(self, *args, **kwargs):
-        print(func)
-        if not self._session:
-            raise ConnectionError(failed_message or 'You must be connected to do this')
-        else:
-            func(*args, **kwargs)
-
-    return magic
-
-
 class LeopardWebScraper(BaseScraper):
     name = 'Leopard Web Scraper'
     simple = 'wit'
@@ -76,14 +55,12 @@ class LeopardWebScraper(BaseScraper):
         self._session = requests.Session()
         return True
 
-    @requires_connection
     def authenticate(self, username='', password=''):
         # WIT makes this complicated and requires that a specific identifier is sent along.
         # This identifier is uniquely generated per request and lives in the login page.
         # We need to scrape this out, along with other possible information, before we can post.
         # if not self._session:
         #     raise ConnectionError('You must be connected to authenticate')
-
         name = ' for ' + self.name if self.name else ''
         if not username:
             username = input('Enter username' + name + ': ')
@@ -114,8 +91,6 @@ class LeopardWebScraper(BaseScraper):
         self._is_auth = True
         return True
 
-    @requires_connection
-    @requires_auth
     def scrape_data(self, outfile_name=''):
         payload = parse.parse_qs(self.get_qs(), keep_blank_values=True)
 
@@ -137,6 +112,8 @@ class LeopardWebScraper(BaseScraper):
             d = self.cleanup_entry(dict(zip(headers, [t.text for t in c.find_all('td', class_='dddefault')])))
             data.append(d)
 
+        data = self.cleanup_data(data)
+
         if outfile_name:
             with open(outfile_name, 'w') as fp:
                 fp.write(json.dumps(data))
@@ -148,6 +125,12 @@ class LeopardWebScraper(BaseScraper):
         class_row['attribute'] = class_row['attribute'].strip()
         return class_row
 
+    def cleanup_data(self, data):
+        for entry in data:
+            del entry['select']
+
+        return data
+
     def __del__(self):
         pass
 
@@ -158,6 +141,5 @@ if __name__ == '__main__':
     # pw = getpass('Enter password: ')
 
     scraper = LeopardWebScraper()
-    scraper.authenticate()
-    # if scraper.connect() and scraper.authenticate(un):
-    #     scraper.scrape_data('wit.json')
+    if scraper.connect() and scraper.authenticate():
+        scraper.scrape_data('wit.json')